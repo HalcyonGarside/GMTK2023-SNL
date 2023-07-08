e6634215844a43f78c9f59273864bded7be5e558
HalcyonGarside <wil.s.blanchard@gmail.com>
2023-07-08 18:31:10 -0400

    Generate Snake

--- Assets/Scripts/SnakeGenerator.cs
+++ Assets/Scripts/SnakeGenerator.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SnakeGenerator : MonoBehaviour
 {
     [SerializeField] private Snake[] _snakeTypes;
+    [SerializeField] private SpriteRenderer _renderer;
 
     // Start is called before the first frame update
     void Start()
@@ -20,12 +21,16 @@ public class SnakeGenerator : MonoBehaviour
 
     public Snake[] generateSnakes(int numSnakes)
     {
-        // var retSnakes = new Snake[numSnakes];
+        var retSnakes = new Snake[numSnakes];
+        var region = _renderer.bounds;
 
-        // for(int snake = 0; snake < retSnakes.Length; snake++)
-        // {
-        //     //retSnakes[snake] = Instantiate(_snakeTypes[Random.Range(0, _snakeTypes.Length)], this.gameObject.transform.)
-        // }
+        for(int snake = 0; snake < retSnakes.Length; snake++)
+        {
+            retSnakes[snake] = Instantiate(_snakeTypes[Random.Range(0, _snakeTypes.Length)], 
+                new Vector3(Random.Range(region.min.x, region.max.x), Random.Range(region.min.y, region.max.y), -4), 
+                Quaternion.identity);
+        }
+
         return new Snake[numSnakes];
     }
 }
